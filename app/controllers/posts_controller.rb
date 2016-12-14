719cc52f5bfdd780dbf3df0d83e59466453b57ee
Christian Perez <idchris@Christians-MBP.home>
2016-12-14 13:12:46 -0800

    finishes tutorial

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,30 @@
+class PostsController < ApplicationController
+
+  def index
+    posts = Post.all
+    if params[:filter]
+      posts = posts.where(["category = ?", params[:filter]])
+    end
+    if params['sort']
+      f = params['sort'].split(',').first
+      field = f[0] == '-' ? f[1..-1] : f
+      order = f[0] == '-' ? 'DESC' : 'ASC'
+      if Post.new.has_attribute?(field)
+        posts = posts.order("#{field} #{order}")
+      end
+    end
+    posts = posts.page(params[:page] ? params[:page][:number] : 1)
+    render json: posts, meta: pagination_meta(posts), include: ['user']
+  end
+
+  private
+  def pagination_meta(object)
+    {
+      current_page: object.current_page,
+      next_page: object.next_page,
+      prev_page: object.previous_page,
+      total_pages: object.total_pages,
+      total_count: object.total_entries
+    }
+  end
+end
---
+++ db/migrate/20161208001158_create_posts.rb
@@ -0,0 +1,19 @@
+# class CreatePosts < ActiveRecord::Migration[5.0]
+#   def change
+#     create_table :posts do |t|
+#     end
+#   end
+# end
+
+class CreatePosts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :posts do |t|
+      t.timestamps
+      t.string     :title
+      t.text       :content
+      t.integer    :user_id
+      t.string     :category
+      t.integer    :rating
+    end
+  end
+end
